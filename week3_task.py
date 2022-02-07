19800dff20396150c30b705c400100cb1354404c
unknown <rachellindsay71@gmail.com>
2022-02-07 21:04:48 +0000

    week3 task

---
+++ week3_task.py
@@ -0,0 +1,13 @@
+#This program lets the user input a string
+#and returns every second letter of their sentence in reverse order
+#resources : https://www.w3schools.com/python/python_howto_reverse_string.asp
+# https://realpython.com/reverse-string-python/#reversing-strings-through-slicing
+
+
+txt = input("please enter a sentence") 
+
+reversetxt = txt [-2::-2]
+
+print(reversetxt)
+
+ 
