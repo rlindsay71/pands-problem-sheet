f36c18cefcb0dd444fea65b538bc6b3ee0cea4f9
unknown <rachellindsay71@gmail.com>
2022-03-09 22:47:20 +0000

    week 7 task count instances of letter in file

---
+++ week7_task.py
@@ -0,0 +1,40 @@
+#  The following Program reads a specified file and will return the count of a given letter.
+#  resource: www.geeksforgeeks.org/count-the-number-of-times-a-letter-appears-in-a-text-file-in-python/
+#  www.w3schools.com/python/python_file_open.asp
+#  pythonexamples.org/python-count-occurrences-of-word-in-text-file/
+#  stackoverflow.com/questions/8280250/how-to-open-files-given-as-command-line-arguments-in-python
+#  Author: Rachel Lindsay
+
+# the following imports the sys library which allows the user to input the filename to be read
+# from the commandline
+
+import sys
+
+# this code takes in the file name as a command line arguement.
+file_name = sys.argv[1]
+# the file is opened here to be read
+f = open(file_name, 'r')
+# here, the file is stored in a variable
+data = f.read()
+# The count function is used to count the frequency of e 
+lettercounter = data.count('e')
+
+print("The number of instances of the letter e is: ", lettercounter)
+
+
+
+
+
+#def letterCounter(fileName, letter):
+    # open file in read mode
+   # file = open(fileName, 'r')
+ 
+    # store content of the file in a variable
+    #TextFile = file.read()
+ 
+    # using count()
+    #return TextFile.count(letter)
+ 
+ 
+# code to reference the function and display the letter count
+#print(letterCounter('gfg.txt', 'f'))
