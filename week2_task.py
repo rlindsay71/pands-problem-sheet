5c2b9cd5892833b796195808aa35c5b21264037c
unknown <rachellindsay71@gmail.com>
2022-03-23 20:40:01 +0000

    BMI Calculator V2

--- week2_task.py
+++ week2_task.py
@@ -1,20 +1,45 @@
-# This program is for calculating a person Body Mass Index (BMI)
+# This program is for calculating a persons Body Mass Index (BMI)
 # based on their weight in KG and height in meters.
 # Author : Rachel Lindsay
-# Week 2, weekly task, Programming & Scripting course
+# resources:  
+# https://www.calculator.net/bmi-calculator.html
+# https://www.w3resource.com/python-exercises/python-basic-exercise-66.php
+# https://docs.python.org/3/tutorial/inputoutput.html
 
-myWeight = float(input(" what is your weight in kilograms?"))
-myHeight = float(input ("what is your height in meters?"))
+ 
+ # The code below asks the user to input their height(kg) and weight(m) and assigns them to variables.
 
-# This code squares the height input
+myWeight = float(input(" what is your weight in kilograms? "))
+myHeight = float(input ("what is your height in meters? "))
 
-myHeightsquared = myHeight ** 2
+# The code below calculates the BMI weight multiplied by height squared
+# and rounds the result to two places.
 
-# This code calculates the BMI
+myBMI = round(myWeight / (myHeight **2), 2)
 
-myBMI = myWeight // myHeightsquared
+# The following code prints out the persons BMI
 
-print(" Your BMI is " + str(myBMI))
+print(f"Your BMI is {myBMI}")
+
+# This code uses if and elif statements to decide what range their BMI belongs do and
+# prints out the result.
+
+if myBMI <= 18.4:
+    print("You are underweight.")
+elif myBMI <= 24.9:
+    print("You are healthy.")
+elif myBMI <= 29.9:
+    print("You are over weight.")
+elif myBMI <= 34.9:
+    print("You are severely over weight.")
+elif myBMI <= 39.9:
+    print("You are obese.")
+else:
+    print("You are severely obese.")
+
+
+
+
 
 
 
