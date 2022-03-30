4dfa544fd89bbaa045cf129a22c8f9e0be55a97b
unknown <rachellindsay71@gmail.com>
2022-03-30 21:46:07 +0100

    V2 week 3 task

--- week3_task.py
+++ week3_task.py
@@ -1,12 +1,16 @@
-#This program lets the user input a string
-#and returns every second letter of their sentence in reverse order
-#resources : https://www.w3schools.com/python/python_howto_reverse_string.asp
+# This program lets the user input a string
+# and returns every second letter of their sentence in reverse order
+# resources : https://www.w3schools.com/python/python_howto_reverse_string.asp
 # https://realpython.com/reverse-string-python/#reversing-strings-through-slicing
 
+# the following code asks the user to imput a string
+txt = input("please enter a sentence: ") 
 
-txt = input("please enter a sentence:\n") 
+# the following code creates a new variable from the original string entered which is reversed and outputs
+# every second letter
 
-reversetxt = txt [-2::-2]
+reversetxt = txt [-1::-2]
+
 
 print(reversetxt)
 
